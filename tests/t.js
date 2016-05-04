d9134a99a30d33a411c9cd03c3614495ab73f406
Rachel Pinsker <rachel.pinsker@gmail.com>
2016-05-04 00:38:16 -0500

    wrote tests for changing names and titles of tune

--- tests/t.js
+++ tests/t.js
@@ -1,31 +1,78 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-});
 
-// outline of tests
+/* changing title, adding name, and removing name
+   all work as follows:
+    - after the user inputs a string to change the title, add a name,
+      or remove a name, the backend handles testing the constraints of
+      that input and updates the object accordingly.
+    - the frontend then receives the new Tune object and a boolean as to whether
+      the Tune object was updated (ie if the input matches the constraints or if
+      the update was a failure).
+    - the frontend uses the functions changeHTML<...> with the boolean to then update
+      the html text boxes. these tests are just testing whether the pages display the
+      correct tune titles or names after changeHTMLTuneTitle is called with either the
+      true or false input
+ */
 
-describe("changing title", function()) {
+describe("changing title", function() {
   beforeEach(function() {
     setUpHTMLFixture();
   });
 
-  changeHTMLTuneTitle(true); // takes a boolean isSuccess
+  changeHTMLTuneTitle(true); // takes a boolean isSuccess based on if the change was successful or not (ie does new title fit constraints?)
+                             // the title is changed if isSuccess is true
 
-  it("valid title change", function() {
-    expect($('#tuneTitle').toHaveText(getTuneTitle()));
+  it("valid title change renders in html", function() {
+    expect($('#tuneTitle').toHaveText(getTuneTitle())); // getTuneTitle() will be a function that returns the tune title from the backend's response after updating the Tune object
   });
 
   changeHTMLTuneTitle(false);
-  it("invalid title change", function()) {
-    expect($('#tuneTitle').toHaveText(getTuneTitle()));
 
-  }
+  it("invalid title change does not change html", function() {
+    expect($('#tuneTitle').toHaveText(getTuneTitle())); // the title should still match the tune object's title
+  });
+});
+
+describe("adding name", function() {
+  beforeEach(function() {
+    setUpHTMLFixture();
+  });
+
+  changeHTMLName(true); // takes a boolean isSuccess based on if the change was successful or not (ie does new name fit constraints?)
+                         // the name is added if isSuccess is true
+
+  it("valid name change renders in html", function() {
+    expect($('#names').toHaveText(getTuneContributors()));
+  });
+
+  changeHTMLName(false);
+
+  it("invalid name change does not change html", function() {
+    expect($('#names').toHaveText(getTuneContributors())); // the names should still match the tune object's contributors
+  });
+
+});
+
+describe("removing name", function() {
+  beforeEach(function() {
+    setUpHTMLFixture();
+  });
+
+  changeHTMLName(true); // takes a boolean isSuccess based on if the deletion was successful or not (ie was the name the user wanted to delete actually present?)
+                         // the name is removed if isSuccess is true
+
+  it("valid name change renders in html", function() {
+    expect($('#names').toHaveText(getTuneContributors()));
+  });
+
+  changeHTMLName(false);
+
+  it("invalid name change does not change html", function() {
+    expect($('#names').toHaveText(getTuneContributors())); // the names should still match the tune object's contributors
+  });
 });
 
 
-describe("uploading midi", function()) {
+describe("uploading midi", function() {
   // uses some code from https://www.noppanit.com/javascript-tdd-on-file-upload/
   // this post was helpful as well http://stackoverflow.com/questions/4234589/validation-of-file-extension-before-uploading-file
   var eventListener = {};
@@ -93,7 +140,7 @@ describe("uploading midi", function()) {
 });
 
 
-describe("downloading pdf", function()) {
+describe("downloading pdf", function() {
 
   var eventListener = {};
   fakeFile = {name : "test.pdf"};
@@ -106,14 +153,14 @@ describe("downloading pdf", function()) {
   displayHTMLTune(true); // takes a boolean isSuccess
 
   it("tune file successfully uploaded", function() {
-    expect($('#tuneTitle').not.toBeNull();
+    expect($('#tuneTitle').not.toBeNull());
   });
 
   displayHTMLTune(true);
 
-  it("invalid title change", function()) {
-    expect($('#tuneTitle').not.toBeNull();
-  }
+  it("invalid title change", function() {
+    expect($('#tuneTitle').not.toBeNull());
+  });
 
 
 });
